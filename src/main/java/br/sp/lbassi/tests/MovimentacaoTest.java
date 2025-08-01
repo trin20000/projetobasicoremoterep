b94365612445f658653419dc0671cbe5f3b4f300
trin20000 <trin20000@gmail.com>
2025-07-31 22:43:41 -0300

    movimentacao futura

--- src/main/java/br/sp/lbassi/tests/MovimentacaoTest.java
+++ src/main/java/br/sp/lbassi/tests/MovimentacaoTest.java
@@ -1,6 +1,9 @@
 package br.sp.lbassi.tests;
 
+import static br.sp.lbassi.utils.DataUtils.obterDataFormatada;
+
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Assert;
@@ -9,6 +12,7 @@ import org.junit.Test;
 import br.sp.lbassi.core.BaseTest;
 import br.sp.lbassi.pages.MenuPage;
 import br.sp.lbassi.pages.MovimentacaoPage;
+import br.sp.lbassi.utils.DataUtils;
 
 public class MovimentacaoTest extends BaseTest {
 	private MenuPage menuPage = new MenuPage();
@@ -41,4 +45,24 @@ public class MovimentacaoTest extends BaseTest {
 		Assert.assertEquals(6, erros.size());
 	}
 
+	@Test
+	public void inserirMovimentacaoFutura() {
+		menuPage.acessarTelaInserirMovimentacao();
+		
+		Date dataFutura = DataUtils.obterDataComDiferencaDias(5);
+		
+		movPage.setDataMovimentacao(obterDataFormatada(dataFutura));
+		movPage.setDataPagamento(obterDataFormatada(dataFutura));
+		movPage.setDescricao("Movimentaçao do Teste");
+		movPage.setInteressado("Interessado Qualquer");
+		movPage.setValor("500");
+		movPage.setConta("Conta do Teste alterada2");
+		movPage.setStatusPago();
+		movPage.salvar();
+		
+		List<String> erros = movPage.obterErros();
+		Assert.assertTrue(erros.contains("Data da Movimentação deve ser menor ou igual à data atual"));
+		Assert.assertEquals(1, erros.size());
+		
+	}
 }
